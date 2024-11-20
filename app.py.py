07628ea2280e24cd73980c948abe08962936e40d
Saswata Bhattacharyya <99406836+Rikihehe@users.noreply.github.com>
2024-11-20 11:05:10 +0530

    Update app.py.py

--- app.py.py
+++ app.py.py
@@ -5,7 +5,7 @@
 
 
 import pandas as pd
-dftrain=pd.read_csv("Titanic_train.csv")
+dftrain=pd.read_csv(r"Titanic_train.csv")
 dftrain
 dftrain=dftrain.drop('Cabin',axis=1)
 dftrain_cleaned=dftrain.dropna()
